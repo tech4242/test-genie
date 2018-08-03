4b50b619fc8b43ba33ded52470854522e1936f16
souhaibbenfarhat <souhaib.benfarhat@esprit.tn>
2018-08-03 17:59:45 +0200

    creating static files and getting responses from the server

--- src/app.go
+++ src/app.go
@@ -3,11 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+var _directory = ""
 
 func main() {
 	const (
@@ -31,8 +41,36 @@ func main() {
 	proxy := &httputil.ReverseProxy{Director: director}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
+		_directory = r.URL.Path
+		proxy.ServeHTTP(NewCustomWriter(w), r)
 	})
 
 	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+type customWriter struct {
+	http.ResponseWriter
+}
+
+func NewCustomWriter(w http.ResponseWriter) *customWriter {
+	return &customWriter{w}
+}
+
+func (c *customWriter) Header() http.Header {
+	return c.ResponseWriter.Header()
+}
+
+func (c *customWriter) Write(data []byte) (int, error) {
+	fmt.Println(string(data))       //get response here
+	fmt.Println(string(_directory)) //get response here
+
+	err := ioutil.WriteFile("src/responses/"+strings.Replace(_directory, "/", "-", -1)+".json", data, 0777)
+	if err != nil {
+		panic(err)
+	}
+	return c.ResponseWriter.Write(data)
+}
+
+func (c *customWriter) WriteHeader(i int) {
+	c.ResponseWriter.WriteHeader(i)
 }
