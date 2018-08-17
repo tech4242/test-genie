f85183a30506870f54afdd2b732bb45955c788ac
souhaibbenfarhat <souhaib.benfarhat@esprit.tn>
2018-08-17 15:19:24 +0200

    fixing the writer

--- src/file_system_manager.go
+++ src/file_system_manager.go
@@ -5,14 +5,7 @@ import (
 )
 
 func writeToFileSystem(data []byte, _directory string, fileName string, fileExtension string, parentFolder string) (int, error) {
-	if _, err := os.Stat(parentFolder + _directory + fileName + "." + fileExtension); err == nil {
 
-		f, _ := os.OpenFile(parentFolder+_directory+fileName+"."+fileExtension, os.O_APPEND|os.O_WRONLY, 0644)
-		_, writingError := f.Write(data)
-		errorHandler(writingError)
-		return 0, writingError
-
-	}
 	os.MkdirAll(parentFolder+_directory, os.ModePerm)
 	fo, err := os.Create(parentFolder + _directory + fileName + "." + fileExtension)
 	errorHandler(err)
